63254f855a038f616b4d8f64a97b6bf70326fd90
Ming-hsuan Chuang <mchuang@e1z1r4p13.42.us.org>
2019-10-10 04:08:10 -0700

    should i do complex f; further instructions for remaining bonus in notes

--- test.c
+++ test.c
@@ -88,47 +88,120 @@ void test_fail()
 {
 }
 
-int main(int argc, char** argv)
+//int main(int argc, char** argv)
+//{
+//	/**
+//	 *	New features include:
+//	 *	1) %b to print binary
+//	 *	2) ' to print apostrophe in duiop
+//	 *	3) $ for indexing arguments
+//	 *	4) Color
+//	 *	5) star
+//	 *	6) fd
+//
+//	 */
+//
+//	//setlocale(LC_ALL, "");
+//	//wchar_t * str  = L"😀😃";
+//	
+////	printf("{%f}\n", 1444565444646.6465424242242);
+////	ft_printf("{%f}\n", 1444565444646.6465424242242);
+//	
+////	printf("{%f}\n", 0.14445654446466465424242242);
+////	ft_printf("{%f}\n", 0.14445654446466465424242242);
+////	ft_printf("{%f}\n", 1.4445654446466465424242242);
+////	ft_printf("{%f}\n", 14.445654446466465424242242);
+////	ft_printf("{%f}\n", 144.45654446466465424242242);
+////	ft_printf("{%f}\n", 1444.5654446466465424242242);
+////	ft_printf("{%f}\n", 14445.654446466465424242242);
+////	ft_printf("{%f}\n", 144456.54446466465424242242);
+////	ft_printf("{%f}\n", 1444565.4446466465424242242);
+////	ft_printf("{%f}\n", 14445654.446466465424242242);
+////	ft_printf("{%f}\n", 144456544.46466465424242242);
+////	ft_printf("{%f}\n", 1444565444.6466465424242242);
+////	ft_printf("{%f}\n", 14445654446.466465424242242);
+////	ft_printf("{%f}\n", 144456544464.66465424242242);
+//	
+//	double d = 3.4222;
+//	
+//	long long int temp = d;
+//	
+//	char sign = (d >> 63) & 1;
+//	
+//	
+//	
+//	
+//	
+//	
+//	long long int mask = 0xffffffff;
+//	
+//	int a = (int)((temp >> 63) & 1);
+////	test_binary();
+////	test_apostrophe();
+////	test_money();
+//	//test_color();
+////	test_star();
+//	//test_fail();
+//	return 0;
+//}
+
+//#include <stdio.h>
+
+union u_double
 {
-	/**
-	 *	New features include:
-	 *	1) %b to print binary
-	 *	2) ' to print apostrophe in duiop
-	 *	3) $ for indexing arguments
-	 *	4) Color
-	 *	5) star
-	 *	6) fd
+    double  dbl;
+    char    data[sizeof(double)];
+};
 
-	 */
+union u_float
+{
+    float   flt;
+    char    data[sizeof(float)];
+};
 
-	//setlocale(LC_ALL, "");
-	//wchar_t * str  = L"😀😃";
-	
-//	printf("{%f}\n", 1444565444646.6465424242242);
-//	ft_printf("{%f}\n", 1444565444646.6465424242242);
-	
-//	printf("{%f}\n", 0.14445654446466465424242242);
-//	ft_printf("{%f}\n", 0.14445654446466465424242242);
-//	ft_printf("{%f}\n", 1.4445654446466465424242242);
-//	ft_printf("{%f}\n", 14.445654446466465424242242);
-//	ft_printf("{%f}\n", 144.45654446466465424242242);
-//	ft_printf("{%f}\n", 1444.5654446466465424242242);
-//	ft_printf("{%f}\n", 14445.654446466465424242242);
-//	ft_printf("{%f}\n", 144456.54446466465424242242);
-//	ft_printf("{%f}\n", 1444565.4446466465424242242);
-//	ft_printf("{%f}\n", 14445654.446466465424242242);
-//	ft_printf("{%f}\n", 144456544.46466465424242242);
-//	ft_printf("{%f}\n", 1444565444.6466465424242242);
-//	ft_printf("{%f}\n", 14445654446.466465424242242);
-	ft_printf("{%f}\n", 144456544464.66465424242242);
-	
-	
-	
-//	test_binary();
-//	test_apostrophe();
-//	test_money();
-	//test_color();
-//	test_star();
-	//test_fail();
-	return 0;
+static void dump_float(union u_float f)
+{
+    int exp;
+    long mant;
+
+    printf("32-bit float: sign: %d, ", (f.data[0] & 0x80) >> 7);
+    exp = ((f.data[0] & 0x7F) << 1) | ((f.data[1] & 0x80) >> 7);
+    printf("expt: %4d (unbiassed %5d), ", exp, exp - 127);
+    mant = ((((f.data[1] & 0x7F) << 8) | (f.data[2] & 0xFF)) << 8) | (f.data[3] & 0xFF);
+    ft_printf("mant: %b %16ld (0x%06lX)\n", mant, mant, mant);
+}
+
+static void dump_double(union u_double d)
+{
+    int exp;
+    long long mant;
+
+    printf("64-bit float: sign: %d, ", (d.data[0] & 0x80) >> 7);
+    exp = ((d.data[0] & 0x7F) << 4) | ((d.data[1] & 0xF0) >> 4);
+    printf("expt: %4d (unbiassed %5d), ", exp, exp - 1023);
+    mant = ((((d.data[1] & 0x0F) << 8) | (d.data[2] & 0xFF)) << 8) | (d.data[3] & 0xFF);
+    mant = (mant << 32) | ((((((d.data[4] & 0xFF) << 8) | (d.data[5] & 0xFF)) << 8) | (d.data[6] & 0xFF)) << 8) | (d.data[7] & 0xFF);
+    ft_printf("mant: %b %16lld (0x%013llX)\n", mant, mant, mant);
+}
+
+static void print_value(double v)
+{
+    union u_double d;
+    union u_float  f;
+
+    f.flt = v;
+    d.dbl = v;
+
+    printf("SPARC: float/double of %g\n", v);
+//    image_print(stdout, 0, f.data, sizeof(f.data));
+//    image_print(stdout, 0, d.data, sizeof(d.data));
+    dump_float(f);
+    dump_double(d);
+}
+
+int	main()
+{
+	print_value(0.5);
+//	print_value(0.25654);
+//	print_value(-3.244456);
 }
