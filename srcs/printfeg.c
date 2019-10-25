55e8f3ac3c06e7c79e9538a76b5e8c943c110f29
Ming-hsuan Chuang <mchuang@e1z1r4p13.42.us.org>
2019-10-25 16:01:55 -0700

    Testing new branch

--- srcs/printfeg.c
+++ srcs/printfeg.c
@@ -25,8 +25,8 @@ union u_double		get_d_param(t_spec *sp, va_list orig)
 		j = 1;
 		while (j < sp->param)
 		{
-			va_arg(cp, int);
-//			va_arg(cp, void*);
+//			va_arg(cp, int);
+			va_arg(cp, void*);
 			j++;
 		}
 		u_d.dbl = va_arg(cp, double);
@@ -51,8 +51,8 @@ union u_ldouble		get_ld_param(t_spec *sp, va_list orig)
 		j = 1;
 		while (j < sp->param)
 		{
-			va_arg(cp, int);
-//			va_arg(cp, void*);
+//			va_arg(cp, int);
+			va_arg(cp, void*);
 			j++;
 		}
 		u_ld.ldbl = va_arg(cp, long double);
